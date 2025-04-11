d0b348da72c448bf551d5b1c1b7c881e8170f862
Bruno Nogueira <brunonog76418@gmail.com>
2025-04-11 16:28:53 -0300

    Corrige build para funcionar no Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 
 export default defineConfig({
-  root: '.', // garante que o Vite use a raiz do projeto
-  base: './', // adicionado para garantir que os caminhos relativos funcionem corretamente
+  base: '', // importante para build no Vercel
   plugins: [
     react(),
     {
@@ -18,13 +17,6 @@ export default defineConfig({
     global: 'globalThis'
   },
   build: {
-    outDir: 'dist',  // Garante que a pasta 'dist' seja gerada
-    rollupOptions: {
-      input: './index.html' // força o uso do index.html raiz
-    }
-  },
-  server: {
-    // Ajuste o Vercel para rodar o local corretamente
-    port: 5173,  // ou qualquer outra porta que você esteja utilizando localmente
+    outDir: 'dist'
   }
 });

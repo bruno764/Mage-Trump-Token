ca5bdf02adbc642c7b400cca9b5752c95918d820
Bruno Nogueira <brunonog76418@gmail.com>
2025-04-11 18:02:35 -0300

    fix: replace PhantomProvider with WalletConnectionProvider in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { PhantomProvider } from './wallet/PhantomProvider';
+import { WalletConnectionProvider } from './wallet/WalletProvider';
 import { AuthProvider } from './contexts/AuthProvider';
 
 import Home from './pages/Home';
@@ -8,10 +8,11 @@ import Admin from './pages/Admin';
 import Login from './pages/Login';
 import Airdrop from './pages/Airdrop';
 import Referrals from './pages/Referrals';
+import About from './pages/About';
 
 export default function App() {
   return (
-    <PhantomProvider>
+    <WalletConnectionProvider>
       <BrowserRouter>
         <AuthProvider>
           <Routes>
@@ -20,9 +21,10 @@ export default function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/airdrop" element={<Airdrop />} />
             <Route path="/referrals" element={<Referrals />} />
+            <Route path="/about" element={<About />} />
           </Routes>
         </AuthProvider>
       </BrowserRouter>
-    </PhantomProvider>
+    </WalletConnectionProvider>
   );
 }

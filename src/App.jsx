1bcde8b96d5b8b09764cc1f9d5ad52c3a034cf13
Bruno Nogueira <brunonog76418@gmail.com>
2025-04-11 17:23:52 -0300

    fix: PhantomProvider scoped globally in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { PhantomProvider } from './wallet/PhantomProvider';
-import { AuthProvider } from './contexts/AuthProvider'; // Garantindo que AuthProvider seja envolvido corretamente
+import { AuthProvider } from './contexts/AuthProvider';
 
 import Home from './pages/Home';
-import Admin from './pages/Admin'; // Adicionando o Admin
+import Admin from './pages/Admin';
 import Login from './pages/Login';
 import Airdrop from './pages/Airdrop';
 import Referrals from './pages/Referrals';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <PhantomProvider>
-        <AuthProvider> {/* AuthProvider agora está dentro do BrowserRouter */}
+    <PhantomProvider>
+      <BrowserRouter>
+        <AuthProvider>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/admin" element={<Admin />} /> {/* Rota para Admin */}
+            <Route path="/admin" element={<Admin />} />
             <Route path="/login" element={<Login />} />
             <Route path="/airdrop" element={<Airdrop />} />
             <Route path="/referrals" element={<Referrals />} />
           </Routes>
         </AuthProvider>
-      </PhantomProvider>
-    </BrowserRouter>
+      </BrowserRouter>
+    </PhantomProvider>
   );
 }
